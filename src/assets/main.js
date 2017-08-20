dc99899a345c03f7309c652b01dbe127e3f3ed3c
Mohammed Ahmed <mohammed_ahmed4949@yahoo.com>
2017-08-20 17:29:56 +0200

    Filling content

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,7 +5,9 @@ $(function() {
       dataType: 'jsonp',
       success: function(data){
           data.courses.completed.forEach(function(course){
-              $("#badges").append('<div class="course"></div>');
+              $("#badges").append('<div class="course"><h3>'
+                                  + course.title + 
+                                  '</h3><img src="' + course.badge + '"> <a href="' + course.url + '" target="_blank" class="btn btn-primary">See Course</a></div>');
           });
       }
   });
